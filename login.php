b9711078afac90a57536e29ac39eba59685ef38d
Farley Gonzalez <gonzalezfarley@hotmail.com>
2023-08-16 08:45:52 -0500

    act 18 agosto-8:45a.m.

--- login.php
+++ login.php
@@ -1,13 +1,28 @@
 <?php
+session_start();
+
+require 'database.php'; 
+
+$resultados = [];
 
 //validamos registro
 if(!empty($_POST['email']) && !empty($_POST['password'])){
     $consulta = $conexion->prepare('SELECT id, email, password FROM usuarios WHERE email=:email');
     $consulta->bindParam(':email', $_POST['email']);
     $consulta->execute();
-    $resutado = $consulta->fetch(PDO::FETCH_ASSOC);
+    $resultados = $consulta->fetch(PDO::FETCH_ASSOC);
 }
 
+$mensaje = "";
+
+    //validar el ingreso del usuario
+    if(count($resultados) > 0 && password_verify($_POST['password'], $resultados['password'])){
+        $_SESSION['user_id'] = $resultados['id'];
+        header('Location: /RegistroBijao');
+        exit();
+    }else{
+        $mensaje = 'Lo sentimos sus credenciales no son correctas';
+    }
 
 ?>
 
@@ -21,6 +36,10 @@ if(!empty($_POST['email']) && !empty($_POST['password'])){
 </head>
 <body>
     <?php require 'partials/header.php'?>
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
     
     <h1>Login</h1>
     <form action="login.php" method="post">
